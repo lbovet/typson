fdfadb090379a61fba9e2b63d352975c530567b1
Liat Rudner <liat.rudner@forter.com>
2022-08-31 16:09:32 +0300

    add tests

---
+++ test/spec/forter-lean-portal/main.ts
@@ -0,0 +1,29 @@
+/**
+ * The request object containing information about the customer and order needed by Forter in order to provide a decision
+ * @metadata.endpoint /v2/orders/:id
+ * @metadata.method ["POST"]
+ */
+interface Transaction {
+    /**
+     * Unique order/transaction identifier
+     * @default 2356fdse0rr489
+     * @maxLength 40
+     * @_leanRequired
+     */
+    orderId: string;
+    /**
+     * @_hiddenFor #1234
+     */
+    orderType: string;
+    /**
+     * @_visibleFor preauth
+     * @_optionalFor magento2
+     * @_hiddenByDefault
+     */
+    authorizationStep: string
+    /**
+     * @_requiredIfAvailable
+     * @_useForExample
+     */
+    accountOwner?: string
+}
